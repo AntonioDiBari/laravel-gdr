6fa6ee8d8ddbd5e99d05f64cbfd3e3dcbd5c2b45
Luca10992 <lucalillo.99.1@gmail.com>
2024-03-27 17:42:31 +0100

    feat: add show, update CRUD

--- resources/views/characters/edit.blade.php
+++ resources/views/characters/edit.blade.php
@@ -1,48 +1,49 @@
 @extends('layouts.app')
 
-@section('title', 'Aggiungi personaggio')
+@section('title', 'Modifica personaggio')
 
 @section('main-content')
   <section>
     <div class="container">
         
-        <h1>Aggiungi Personaggio</h1>
-        <form action="{{ route('characters.store')}}" method="POST" class='row'>
+        <h1>Modifica Personaggio</h1>
+        <form action="{{ route('characters.update', $character)}}" method="POST" class='row'>
         @csrf
+        @method('PATCH')
         <div class="mb-3 col-3">
             <label for="name" class="form-label">Name</label>
             <div class="input-group">
-                <input type="text" required class="form-control" id="name" aria-describedby="basic-addon3 basic-addon4" name='name'>
+                <input type="text" required class="form-control" value="{{ $character->name }}" id="name" aria-describedby="basic-addon3 basic-addon4" name='name'>
             </div>
         </div>
         <div class="mb-3 col-3">
             <label for="attack" class="form-label">attack</label>
             <div class="input-group">
-                <input type="number" required class="form-control" id="attack" aria-describedby="basic-addon3 basic-addon4" name='attack'>
+                <input type="number" required class="form-control" value="{{ $character->attack }}" id="attack" aria-describedby="basic-addon3 basic-addon4" name='attack'>
             </div>
         </div>
         <div class="mb-3 col-3">
             <label for="defence" class="form-label">defence</label>
             <div class="input-group">
-                <input type="number" required class="form-control" id="defence" aria-describedby="basic-addon3 basic-addon4" name='defence'>
+                <input type="number" required class="form-control" value="{{ $character->defence }}" id="defence" aria-describedby="basic-addon3 basic-addon4" name='defence'>
             </div>
         </div>
         <div class="mb-3 col-3">
             <label for="speed" class="form-label">speed</label>
             <div class="input-group">
-                <input type="number" required class="form-control" id="speed" aria-describedby="basic-addon3 basic-addon4" name='speed'>
+                <input type="number" required class="form-control" value="{{ $character->speed }}" id="speed" aria-describedby="basic-addon3 basic-addon4" name='speed'>
             </div>
         </div>
         <div class="mb-3 col-3">
             <label for="life" class="form-label">life</label>
             <div class="input-group">
-                <input type="number" required class="form-control" id="life" aria-describedby="basic-addon3 basic-addon4" name='life'>
+                <input type="number" required class="form-control" value="{{ $character->life }}" id="life" aria-describedby="basic-addon3 basic-addon4" name='life'>
             </div>
         </div>
         <div class="mb-3 col-12">
             <label for="description" class="form-label">description</label>
             <div class="input-group">
-            <textarea class="form-control" aria-label="With textarea" name='description'></textarea>
+            <textarea class="form-control" aria-label="With textarea" name='description'>{{ $character->description }}</textarea>
             </div>
         </div>
 
